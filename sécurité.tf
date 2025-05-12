35e2c5b742f7d3a7808e29e8beee96ed4cb8b33a
Isseumayib <125272519+Isseumayib@users.noreply.github.com>
2025-05-12 12:44:57 +0100

    Update sécurité.tf

--- sécurité.tf
+++ sécurité.tf
@@ -7,14 +7,14 @@ resource "aws_security_group" "ec2_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [41.82.181.220]
+    cidr_blocks = [41.83.106.139]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["41.83.106.139/32"]
   }
 
   tags = {
