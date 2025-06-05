b2a41483103167ef527ccfbcd82199d5fdd2fa70
Isseumayib <125272519+Isseumayib@users.noreply.github.com>
2025-06-05 17:01:45 +0100

    Update sécurité.tf

--- sécurité.tf
+++ sécurité.tf
@@ -1,23 +1,44 @@
 resource "aws_security_group" "ec2_sg" {
-  name        = "groupe-ssh-projet"
-  description = "Autorise uniquement SSH depuis une IP spécifique"
+  name        = "groupe-ssh-http-https-projet"
+  description = "Autorise SSH depuis une IP spécifique, HTTP et HTTPS en accès public"
   vpc_id      = aws_vpc.main.id
 
+  # SSH (port 22) uniquement depuis ton IP
   ingress {
+    description = "SSH depuis une IP spécifique"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [41.83.106.139]
+    cidr_blocks = ["41.83.106.139/32"]
   }
 
+  # HTTP (port 80) depuis partout
+  ingress {
+    description = "HTTP public"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTPS (port 443) depuis partout
+  ingress {
+    description = "HTTPS public"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Autoriser tout le trafic sortant
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["41.83.106.139/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name = "SG-SSH-Projet"
+    Name = "SG-SSH-HTTP-HTTPS-Projet"
   }
 }
